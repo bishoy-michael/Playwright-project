c49e465dd1c3b67709cf5bc3ad1a72d8faf91d35
Bishoy Michael <bishoy.michael@capgemini.com>
2026-09-25 14:39:14 +0300

    Config using .env file and add test cases

---
+++ tests/ui-components.spec.ts
@@ -0,0 +1,216 @@
+import { expect, test } from '@playwright/test'
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('/')
+})
+
+test.describe('Form Layouts page', () => {
+    test.describe.configure({retries: 2})
+    
+    test.beforeEach(async ({ page }) => {
+        await page.getByText('Forms').click()
+        await page.getByText('Form Layouts').click()
+    })
+
+    test('Input fields', {tag: ['@smoke', '@fields']}, async({page}, testInfo) => {
+        if(testInfo.retry){
+            //clean test data
+        }
+        const usingTheGridEmailInput = page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"})
+        await usingTheGridEmailInput.fill('test@test.com')
+        await usingTheGridEmailInput.clear()
+        await usingTheGridEmailInput.pressSequentially('test2@test.com', {delay: 500})
+
+        //extract the value
+        const inputValue = await usingTheGridEmailInput.inputValue()
+
+        //assertions
+        await expect(usingTheGridEmailInput).toHaveValue('test2@test.com')
+        await expect(usingTheGridEmailInput).toHaveValue(/test.com/)
+
+    })
+
+    test('radio buttons', {tag: ['@smoke', '@radio']}, async({page}) => {
+        const usingTheGridForm = page.locator('nb-card', {hasText: "Using the Grid"})
+
+        await usingTheGridForm.getByLabel('Option 2').check({force: true})
+        await usingTheGridForm.getByRole('radio', {name: "Option 1"}).check({force: true})
+
+        const radioStatus = await usingTheGridForm.getByRole('radio', {name: "Option 1"}).isChecked()
+        expect(radioStatus).toBeTruthy()
+
+        await expect(usingTheGridForm.getByRole('radio', {name: "Option 1"})).toBeChecked()
+        await expect(usingTheGridForm.getByRole('radio', {name: "Option 2"})).not.toBeChecked()
+    })
+})
+
+test('checkboxes', async({page}) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Toastr').click()
+
+    await page.getByRole('checkbox', {name: 'Hide on click'}).uncheck({force: true})
+
+    const allBoxes = page.getByRole('checkbox')
+    for(const box of await allBoxes.all()){
+        await box.check({force: true})
+        await expect(box).toBeChecked()
+    }
+})
+
+test('Lists and dropdowns', async({page}) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Toastr').click()
+
+    //standard dropdown
+    await page.locator('.form-group', {hasText: 'Toast type:'}).getByRole('combobox').selectOption('info')
+    await expect(page.getByRole('combobox')).toHaveValue('info')
+
+    //custom dropdowns
+    await page.locator('.form-group', {hasText: 'Position:'}).locator('nb-select').click()
+    //option1
+    //await page.getByRole('list').getByText('bottom-end').click()
+    //option2
+    await page.locator('nb-option', {hasText: "bottom-end"}).click()
+    await expect(page.locator('.form-group', {hasText: 'Position:'}).locator('nb-select')).toHaveText('bottom-end')
+
+    //looping through the list
+    const positionDropDownField = page.locator('.form-group', {hasText: 'Position:'}).locator('nb-select')
+    await positionDropDownField.click()
+    const allListValues = await page.locator('nb-option').allTextContents()
+    for (const listValue of allListValues){
+        await page.locator('nb-option', {hasText: listValue}).click()
+        await expect(positionDropDownField).toHaveText(listValue)
+        await positionDropDownField.click()
+    }
+})
+
+test('tooltips', async ({ page }) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Tooltip').click()
+
+    await page.getByRole('button', {name: 'Top'}).hover()
+    await expect(page.getByRole('tooltip')).toHaveText('This is a tooltip')
+})
+
+test('dialog box', async ({ page }) => {
+    await page.getByText('Tables & Data').click()
+    await page.getByText('Smart Table').click()
+
+    page.on('dialog', dialog => {
+        expect(dialog.message()).toEqual('Are you sure you want to delete?')
+        dialog.accept()
+    })
+
+    await page.locator('tr', {hasText: 'mdo@gmail.com'}).locator('.nb-trash').click()
+    await expect(page.locator('tr', {hasText: 'mdo@gmail.com'})).not.toBeVisible()
+})
+
+test('web tables', async ({ page }) => {
+    await page.getByText('Tables & Data').click()
+    await page.getByText('Smart Table').click()
+
+    //1 how to select row by any visible text
+    const tableRowByEmail = page.getByRole('row', {name: 'twitter@outlook.com'})
+    await tableRowByEmail.locator('.nb-edit').click()
+    await tableRowByEmail.getByPlaceholder('Age').fill('35')
+    await tableRowByEmail.locator('.nb-checkmark').click()
+    await expect(tableRowByEmail.locator('td').last()).toHaveText('35')
+
+    //2 get row by a spcific column value
+    const tableRowById = page.getByRole('row').filter({has: page.getByRole('cell').nth(1).getByText('10')})
+    await tableRowById.locator('.nb-edit').click()
+    await page.locator('tbody').getByPlaceholder('E-mail').fill('test@test.com')
+    await page.locator('tbody').locator('.nb-checkmark').click()
+    await expect(tableRowById.locator('td').nth(5)).toHaveText('test@test.com')
+
+    //3 loop through table rows
+
+    const ages = ["20", "30", "40", "200"]
+
+    for( let age of ages){
+        await page.getByPlaceholder('Age').fill(age)
+
+        if(age == "200"){
+            await expect(page.locator('tbody')).toContainText('No data found')
+        } else {
+            await expect(page.locator('tbody tr').first().locator('td').last()).toHaveText(age)
+            const allTableRows = await page.locator('tbody tr').all()
+            for(let row of allTableRows){
+                await expect(row.locator('td').last()).toHaveText(age)
+            }
+
+        }
+    }
+})
+
+test('datepicker', async ({ page }) => {
+    await page.getByText('Forms').click()
+    await page.getByText('Datepicker').click()
+
+    const calendarInputField = page.getByPlaceholder('Form Picker')
+    await calendarInputField.click()
+
+    const date = new Date();
+    date.setDate(date.getDate() + 100)
+    const expectedDay = date.getDate().toString()
+    const expectedMonth = date.toLocaleString('En-US', {month: 'short'})
+    const expectedMonthLong = date.toLocaleString('En-US', {month: 'long'})
+    const expectedYear = date.getFullYear()
+    const expectedDate = `${expectedMonth} ${expectedDay}, ${expectedYear}`
+
+    let currentMonthAndYear = await page.locator('nb-calendar-view-mode').textContent()
+    const expectedMonthAndYear = `${expectedMonthLong} ${expectedYear}`
+    while(!currentMonthAndYear?.includes(expectedMonthAndYear)){
+        await page.locator('.next-month').click()
+        currentMonthAndYear = await page.locator('nb-calendar-view-mode').textContent()
+    }
+
+    await page.locator('.day-cell:not(.bounding-month)').getByText(expectedDay, { exact: true }).click()
+    await expect(calendarInputField).toHaveValue(expectedDate)
+})
+
+test('sliders', async ({ page }) => {
+    //1 setting the attribute values
+    const tempGauge = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger circle')
+    await tempGauge.evaluate( element => {
+        element.setAttribute('cx', '232.630')
+        element.setAttribute('cy', '232.630')
+    })
+    await tempGauge.click()
+
+    //2 mouse movement
+    const tempBox = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger')
+    await tempBox.scrollIntoViewIfNeeded()
+
+    const box: any = await tempBox.boundingBox()
+    const x = box.x + box.width / 2
+    const y = box.y + box.height / 2
+    await page.mouse.move(x, y)
+    await page.mouse.down()
+    await page.mouse.move(x+100, y)
+    await page.mouse.move(x+100, y+100)
+    await page.mouse.up()
+    await expect(tempBox).toContainText('30')
+})
+
+test('iFrames', async({page}) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Dialog').click()
+
+    const frameLocator = page.frameLocator('[data-cy="esc-close-iframe"]')
+
+    await frameLocator.getByRole('button', {name: 'Open Dialog with esc close'}).click()
+})
+
+test('Drag & drop', async({page}) => {
+    await page.getByText('Extra Components').click()
+    await page.getByText('Drag & Drop').click()
+    //1
+    await page.getByText('Clean my room').dragTo(page.locator('#drop-list'))
+
+    //2
+    await page.getByText('Get groceries').hover()
+    await page.mouse.down()
+    await page.locator('#drop-list').hover()
+    await page.mouse.up()
+})
